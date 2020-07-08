b25d0e753b29902853409f0d85c0f5ae440bc9ce
Priya Power <priyajag72@gmail.com>
2020-07-08 09:12:29 -0600

    Adds readable attribute category.

--- lib/product.rb
+++ lib/product.rb
@@ -1,9 +1,12 @@
 class Product
 
+  attr_reader :category
+  
   def initialize(category, name, unit_price, quantity)
     @category = category
     @name = name
     @unit_price = unit_price
     @quantity = quantity
   end
+
 end
